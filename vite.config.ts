01591b86d99b480dd1d8e51140508761569ecbcf
Avi <76801121+FinexusAvi@users.noreply.github.com>
2024-12-12 23:44:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { componentTagger } from 'lovable-tagger';
+import ssr from 'vite-plugin-ssr/plugin';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: '::',
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
+    ssr(), 
   ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    ssr: true, 
   },
 }));
